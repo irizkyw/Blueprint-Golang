7a1c3391176c8e45a2e78fd533e8ab081b4b7181
Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
2025-02-28 14:17:26 +0700

    template responses controllers

--- internal/controllers/handler/controller_responses.go
+++ internal/controllers/handler/controller_responses.go
@@ -7,6 +7,7 @@ type Response struct {
 	Code    int         `json:"code"`
 	Data    interface{} `json:"data,omitempty"`
 	Error   *string     `json:"error,omitempty"`
+	Message *string     `json:"message,omitempty"`
 }
 
 type Controller struct{}
@@ -16,7 +17,15 @@ func (c *Controller) Success(ctx *fiber.Ctx, data interface{}, code int) error {
 		Success: true,
 		Code:    code,
 		Data:    data,
-		Error:   nil,
+	}
+	return ctx.Status(code).JSON(response)
+}
+
+func (c *Controller) SuccessMessage(ctx *fiber.Ctx, message string, code int) error {
+	response := Response{
+		Success: true,
+		Code:    code,
+		Message: &message,
 	}
 	return ctx.Status(code).JSON(response)
 }
@@ -25,8 +34,32 @@ func (c *Controller) Error(ctx *fiber.Ctx, message string, code int) error {
 	response := Response{
 		Success: false,
 		Code:    code,
-		Data:    nil,
 		Error:   &message,
 	}
 	return ctx.Status(code).JSON(response)
+}
+
+// BadRequest response status 400
+func (c *Controller) BadRequest(ctx *fiber.Ctx, message string) error {
+	return c.Error(ctx, message, fiber.StatusBadRequest)
+}
+
+// Unauthorized response status 401
+func (c *Controller) Unauthorized(ctx *fiber.Ctx, message string) error {
+	return c.Error(ctx, message, fiber.StatusUnauthorized)
+}
+
+// Forbidden response status 403
+func (c *Controller) Forbidden(ctx *fiber.Ctx, message string) error {
+	return c.Error(ctx, message, fiber.StatusForbidden)
+}
+
+// NotFound response status 404
+func (c *Controller) NotFound(ctx *fiber.Ctx, message string) error {
+	return c.Error(ctx, message, fiber.StatusNotFound)
+}
+
+// InternalServerError response status 500
+func (c *Controller) InternalServerError(ctx *fiber.Ctx, message string) error {
+	return c.Error(ctx, message, fiber.StatusInternalServerError)
 }
